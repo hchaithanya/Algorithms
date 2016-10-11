8a058e619c46333f453c0334126090114aef0643
H.Chaithanya <chaitanya7991@gmail.com>
2016-10-11 21:59:12 +0530

    Update kangroo.cpp

--- kangroo.cpp
+++ kangroo.cpp
@@ -1,3 +1,25 @@
+There are two kangaroos on an x-axis ready to jump in the positive direction (i.e, toward positive infinity). The first kangaroo starts at location and moves at a rate of meters per jump. The second kangaroo starts at location and moves at a rate of meters per jump. Given the starting locations and movement rates for each kangaroo, can you determine if they'll ever land at the same location at the same time?
+
+Input Format
+
+A single line of four space-separated integers denoting the respective values of , , , and .
+
+Constraints
+
+Output Format
+
+Print YES if they can land on the same location at the same time; otherwise, print NO.
+
+Note: The two kangaroos must land at the same location after making the same number of jumps.
+
+Sample Input 0
+
+0 3 4 2
+
+Sample Output 0
+
+YES
+
 #include <map>
 #include <set>
 #include <list>
