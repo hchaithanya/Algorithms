fe7246ef95857cf63d8a335a295f254615e4b4a2
hchaithanya <chaitanya7991@gmail.com>
2017-07-14 19:05:23 +0530

    removing duplicate elements from a string

---
+++ interview-questions/duplicate.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+
+void display(char *name, int l)
+{
+	int i;
+	for(i=0;i<l;i++)
+	{
+		cout<<name[i];
+	}
+}
+
+void dupli(char *name, int l)
+{
+	int flag;
+	int i=0,j=0;
+	char temp=name[i];
+	int count;
+	
+	for(i=0;i<l;i++)
+	{
+		for(j=i+1;j<l;j++)
+		{
+			if(name[i]==name[j])
+			{
+			name[i]=name[j+1];
+			}
+		}
+	//name[j]==name[count+1];
+	}
+	for(i=0;i<l;i++)
+	{
+		cout<<name[i];
+	}
+	//display(*name,l);
+}
+
+
+void sort(char *name, int l)
+{
+	int i;
+	char small;
+	int j;
+	for(i=0;i<l;i++)
+		{
+			for(j=i+1;j<l;j++)
+			{
+				if(name[i]>name[j])
+				{
+					small=name[j];
+					name[j]=name[i];
+				 	name[i]=small;
+				 }
+				}
+			}
+
+}
+int main(){
+
+	char word[30];
+	int l;
+	//word= new char;
+	cout<<"Enter the string to find the duplicate\n";
+	gets(word);
+	l=strlen(word);
+	sort(word,l);
+	dupli(word,l);
+}
